1d539c79cac805eaa05a0b5cc57c5387ceca7d86
Bartek <bartek.planiak@gmail.com>
2021-02-17 22:13:30 +0100

    Add bird physics & control

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    private const float JUMP_AMOUNT = 100f;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rigidbody.velocity = Vector2.up * JUMP_AMOUNT;
+    }
+}
