f7e14b16fc9a91440576e118ebeb4a077b6ca251
Bartek <bartek.planiak@gmail.com>
2021-02-19 21:14:54 +0100

    Add bottom & top pipes script

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -10,22 +10,36 @@ public class Level : MonoBehaviour
 
     private void Start()
     {
-        CreatePipe(50f, 20f);
-        CreatePipe(40f, 40f);
-        CreatePipe(30f, 60f);
-        CreatePipe(20f, 80f);
-        CreatePipe(10f, 100f);
+        CreateGapPipes(50f, 30f, 40f);
     }
 
-    private void CreatePipe(float height, float xPosition)
+    private void CreateGapPipes(float gapY, float gapSize, float xPosition){
+        CreatePipe(gapY - gapSize * 0.5f, xPosition, true);
+        CreatePipe(CAMERA_ORTHO_SIZE * 2f - gapY - gapSize * 0.5f, xPosition, false);
+    }
+
+    private void CreatePipe(float height, float xPosition, bool createBottom)
     {
         // Set up Pipe Head
         Transform pipeHead = Instantiate(GameAssets.getInstance().pfPipeHead);
-        pipeHead.position = new Vector2(xPosition, -CAMERA_ORTHO_SIZE + height - PIPE_HEAD_HEIGHT * 0.5f);
+        float pipeHeadYPosition;
+        if (createBottom){
+            pipeHeadYPosition = -CAMERA_ORTHO_SIZE + height - PIPE_HEAD_HEIGHT * 0.5f;
+        } else {
+            pipeHeadYPosition = +CAMERA_ORTHO_SIZE - height + PIPE_HEAD_HEIGHT * 0.5f;
+        }
+        pipeHead.position = new Vector2(xPosition, pipeHeadYPosition);
 
         // Set up Pipe Body
         Transform pipeBody = Instantiate(GameAssets.getInstance().pfPipeBody);
-        pipeBody.position = new Vector2(xPosition, -CAMERA_ORTHO_SIZE);
+        float pipeBodyYPosition;
+        if (createBottom){
+            pipeHeadYPosition = -CAMERA_ORTHO_SIZE;
+        } else {
+            pipeHeadYPosition = +CAMERA_ORTHO_SIZE;
+            pipeBody.localScale = new Vector2(1, -1);
+        }
+        pipeBody.position = new Vector2(xPosition, pipeHeadYPosition);
 
         SpriteRenderer pipeBodySpriteRenderer = pipeBody.GetComponent<SpriteRenderer>();
         pipeBodySpriteRenderer.size = new Vector2(PIPE_WIDTH, height);
